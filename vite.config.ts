c2fb4f1dcef30839d19763fc6bb5ab6f04ee6ccf
Jonhdisk <registez007@gmail.com>
2026-05-14 23:33:12 +0700

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.VITE_BASE_URL ?? '/',
+  base: './',
   plugins: [react()],
   server: {
     port: 5173,
+    proxy: {
+      '/api': 'http://localhost:8080',
+      '/ws': { target: 'ws://localhost:8080', ws: true },
+    },
   },
 })
